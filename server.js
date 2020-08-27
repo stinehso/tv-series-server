19bebd3aaa75eb5b73b16dbf046cf00e87e49fd2
Stine <stinehso@gmail.com>
2020-08-27 13:09:29 +0200

    Move file reading to the api call

--- server.js
+++ server.js
@@ -7,18 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 let seriesList;
-fs.readFile('./Series.txt', (err, data) => {
-    if (err) {
-        console.log(err);
-    }
-    seriesList = data.toString('utf8').split('\r\n')
-})
-
 
 app.get('/', (req, res) => { 
-    res.send(seriesList) 
+    fs.readFile('./Series.txt', (err, data) => {
+        if (err) throw err;
+        seriesList = data.toString('utf8').split('\r\n')
+        res.send(seriesList) 
+    })
 })
 
 
