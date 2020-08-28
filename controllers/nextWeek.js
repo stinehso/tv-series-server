2305d692e8a41ecb0bdc641afc8b2a4d0c96ebeb
Stine <stinehso@gmail.com>
2020-08-29 00:32:59 +0200

    Move nextWeekHandling to separate file

---
+++ controllers/nextWeek.js
@@ -0,0 +1,27 @@
+const fileHandling = require('../fileHandling');
+
+const filterRunning = (table) => {
+    // should take a list of objects and return the ones that hvave status: running
+}
+
+const getNextWeekFields = (table) => {
+
+}
+
+const handleNextWeek = (seriesInfo, res) => {
+    const headerString = 'SHOW_NAME;MONDAY;TUESDAY;WEDNESDAY;THURSDAY;FRIDAY;SATURDAY;SUNDAY'
+    let table = getNextWeekFields(seriesinfo);
+    table = filterRunning(table);
+
+    fileHandling.createFile(headerString, table, 'nextWeek_report.txt')
+    const tableObject = {
+        headers: ['Name', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        keys: Object.keys(table[0]),
+        table: table
+    }
+    res.json(tableObject)
+}
+
+module.exports = {
+    handleNextWeek
+}
