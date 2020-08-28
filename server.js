d7b39c9105403f6f6376bfceec54773aed4eb9dc
Stine <stinehso@gmail.com>
2020-08-28 23:34:30 +0200

    Move TVmaze API call to different file

--- server.js
+++ server.js
@@ -22,36 +22,7 @@ const readFile = () => {
 let seriesList = readFile();
 
 
-async function callTVmaze(show) {
-    let tryAgain = true;
-    let response;
-    while (tryAgain) {
-        response = await fetch(`http://api.tvmaze.com/singlesearch/shows?q=${show}`, {
-            headers: {
-                'User-Agent': 'SeriesReportServer'
-            }
-        })
-            .then(res => res.json())
-            .then(res => {
-            if (res.status === 429) {
-                setTimeout(() => console.log('Trying API again'), 2000);
-            } else {
-                tryAgain = false;
-                return res
-            }
-        }).catch(e => console.log('error:', e))
-    }
-    return response   
-}
 
-async function getShowInfo(seriesList) {
-    let seriesInfo = [];
-    for (const show of seriesList) {
-        let info = await callTVmaze(show)
-        seriesInfo.push(info)
-    }
-    return seriesInfo
-} 
 
 
 
